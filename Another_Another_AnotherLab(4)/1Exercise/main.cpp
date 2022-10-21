f1c6ac81cb0cb3e73cc1e898b94df07903607cb9
Gilsson <antongulis43@gmail.com>
2022-10-21 04:12:00 +0300

    develop

--- Another_Another_AnotherLab(4)/1Exercise/main.cpp
+++ Another_Another_AnotherLab(4)/1Exercise/main.cpp
@@ -1,48 +1,130 @@
 #include <iostream>
 
-/*template <typename T>
-std::istream& operator>>(std::istream& in, T* param)
+long long PrintNum()
 {
-    for (int i = 0;;++i) {
-        std::cin >> *(param++);
-        while(true) {
-            if (in.fail() && (char)in.get() == 0) {
-                in.clear();
-                in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                std::cout << "Wrong input!\n";
+    long long output = 0;
+    bool Sign = true;                   // true = +, false = -
+    while(true) {
+        int size = 0;
+        bool IsWrongInput = false;
+        long long PointIndex = -1;
+        char* input = new char[25];
+        for (int i = 0; i < 25; ++i) {
+            std::cin >> input[i];
+            if (input[i] == 45) {
+                if (i == 0) {
+                    Sign = false;
+                    continue;
+                } else {
+                    std::cout << "Wrong Input\n";
+                    IsWrongInput = true;
+                    break;
+                }
             }
-            if (!in.fail()) {
+            while ((input[i] < int('0') || input[i] > int('9')) && input[i] != int('.')) {
+                std::cout << "Wrong Input\n";
+                IsWrongInput = true;
+                break;
+            }
+
+            if (input[i] == int('.')) {
+                for (int j = 0; j < i; ++j) {
+                    if (input[j] == int('.')) {
+                        std::cout << "Wrong Input!\n";
+                        IsWrongInput = true;
+                    }
+                }
+            }
+            if(PointIndex == -1) {
+                for (int j = 0; j <= size; ++j) {
+                    if (input[j] == int('.'))
+                        PointIndex = j;
+                }
+            }
+            if(PointIndex != -1) {
+                for (int j = PointIndex + 1; j <= size; ++j) {
+                    if (input[j] != int('0')) {
+                        std::cout << "Wrong input!\n";
+                        IsWrongInput = true;
+                        break;
+                    }
+                }
+            }
+            if (!IsWrongInput) {
+                ++size;
+            } else {
+                break;
+            }
+
+            if (std::cin.peek() == '\n') {
                 break;
             }
         }
-        if(*(param) != 0)
+        if(IsWrongInput)
+        {
+            delete[] input;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Please, input the number one more time.\n";
+        }
+        else
+        {
+            if(PointIndex != -1) {
+                for (int i = PointIndex - 1; i >= 0; --i) {
+                    output += (input[i] - '0') * powl(10, PointIndex - i - 1);
+                }
+            }else
+            {
+                for (int i = size - 1; i >= 0; --i) {
+                    output += (input[i] - '0') * powl(10, size - i - 1);
+                }
+
+            }
             break;
+        }
     }
-    return in;
-}*/
+    return output;
+}
 
 
 int main() {
-    const int k = 5;
-    int array[k]{};
-    for(int count = 0; count < k; ++count)
-    {
-        std::cin >> array[count];
-    }
-    int n = 6;
-    for(int i = 0; i < n; ++i)
-    {
-        int tempInt = 0;
-        tempInt = array[k - 1];
-        for(int count = k - 1; count > 0; --count)
-        {
-            array[(count) % k] = array[(count - 1)%k];
+    while(true) {
+        std::cout << "Exercise number 1:\n" << "Made by student of group 253502 Anton Hulis\n"
+                  << "Number in the list is 7\n" << "TASK: Move an elements of an array on the N positions to right.\n";
+        const int k = 5;
+        int array[k]{};
+
+        std::cout << "\n \t Type a " << k << " elements of array.(use Enter to separate each element) \n";
+        for (int count = 0; count < k; ++count) {
+            array[count] = PrintNum();
         }
-        array[0] = tempInt;
-    }
-    for(int count = 0; count < k; ++count)
-    {
-        std::cout << array[count] << " ";
+        int n = 6;
+
+        std::cout << "\n \t Array have " << k << " elements, which are: \n";
+        for (int count = 0; count < k; ++count) {
+            std::cout << array[count] << " ";
+        }
+        std::cout << '\n';
+        for (int i = 0; i < n; ++i) {
+            int tempInt = 0;
+            tempInt = array[k - 1];
+            for (int count = k - 1; count > 0; --count) {
+                array[(count) % k] = array[(count - 1) % k];
+            }
+            array[0] = tempInt;
+        }
+        std::cout << "\n \t Array after moving on " << n << " elements to right: \n";
+        for (int count = 0; count < k; ++count) {
+            std::cout << array[count] << " ";
+        }
+        std::cout << "\n \t \tDo you want to proceed? (if yes, print y, else, print any other symbol\n";
+        char Proceed;
+        std::cin >> Proceed;
+        if(Proceed != 'y')
+        {
+            break;
+        }
+        system("cls");
     }
     return 0;
 }
--- Another_Another_AnotherLab(4)/2Exercise/main.cpp
+++ Another_Another_AnotherLab(4)/2Exercise/main.cpp
@@ -1,15 +1,102 @@
 #include <iostream>
 
+long long PrintNum()
+{
+    long long output = 0;
+    bool Sign = true;                   // true = +, false = -
+    while(true) {
+        int size = 0;
+        bool IsWrongInput = false;
+        long long PointIndex = -1;
+        char* input = new char[25];
+        for (int i = 0; i < 25; ++i) {
+            std::cin >> input[i];
+            if (input[i] == 45) {
+                if (i == 0) {
+                    Sign = false;
+                    continue;
+                } else {
+                    std::cout << "Wrong Input\n";
+                    IsWrongInput = true;
+                    break;
+                }
+            }
+            while ((input[i] < int('0') || input[i] > int('9')) && input[i] != int('.')) {
+                std::cout << "Wrong Input\n";
+                IsWrongInput = true;
+                break;
+            }
+            if (input[i] == int('.')) {
+                for (int j = 0; j < i; ++j) {
+                    if (input[j] == int('.')) {
+                        std::cout << "Wrong Input!\n";
+                        IsWrongInput = true;
+                    }
+                }
+            }
+            if(PointIndex == -1) {
+                for (int j = 0; j <= size; ++j) {
+                    if (input[j] == int('.'))
+                        PointIndex = j;
+                }
+            }
+            if(PointIndex != -1) {
+                for (int j = PointIndex + 1; j <= size; ++j) {
+                    if (input[j] != int('0')) {
+                        std::cout << "Wrong input!\n";
+                        IsWrongInput = true;
+                        break;
+                    }
+                }
+            }
+            if (!IsWrongInput) {
+                ++size;
+            } else {
+                break;
+            }
+            if (std::cin.peek() == '\n') {
+                break;
+            }
+        }
+        if(IsWrongInput)
+        {
+            delete[] input;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Please, input the number one more time.\n";
+        }
+        else
+        {
+            if(PointIndex != -1) {
+                for (int i = PointIndex - 1; i >= 0; --i) {
+                    output += (input[i] - '0') * powl(10, PointIndex - i - 1);
+                }
+            }else
+            {
+                for (int i = size - 1; i >= 0; --i) {
+                    output += (input[i] - '0') * powl(10, size - i - 1);
+                }
+
+            }
+            break;
+        }
+    }
+    return output;
+}
+
+
 int main() {
+    std::cout << "Exercise number 2:\n" << "Made by student of group 253502 Anton Hulis\n"
+              << "Number in the list is 7\n" << "TASK: Find out the largest element which lies under the secondary diagonal.\n";
     const int n = 4, k = 5;
     int matrix[n][k]{};
-    for(int row = 0; row < n; ++row)
+    /*for(int row = 0; row < n; ++row)
     {
         for(int column = 0; column < k; ++column)
         {
             std::cin >> matrix[row][column];
         }
-    }
+    }*/
     for(int row = 0; row < n; ++row)
     {
         for(int column = 0; column < k; ++column)
