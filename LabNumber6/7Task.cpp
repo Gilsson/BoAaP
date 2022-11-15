2bb894e84ab1d463a5167bf4e69e84f34d0ada77
Gilsson <antongulis43@gmail.com>
2022-11-15 22:45:19 +0300

    7Task

--- LabNumber6/7Task.cpp
+++ LabNumber6/7Task.cpp
@@ -1,3 +1,30 @@
-//
-// Created by Gilsson on 11/15/2022.
-//
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str;
+    std::cin >> str;
+    int greatest_size = -1;
+    char buff = str[0];
+    bool flag = false;
+    for(int i = 0, j = str.length() - 1; i <= j; ++i, --j)
+    {
+        if(buff != str[i] || buff != str[j])
+        {
+            flag = true;
+        }
+        if(str[i] != str[j])
+        {
+            greatest_size = str.length();
+            break;
+        }
+    }
+    if(greatest_size == str.length())
+        std::cout << greatest_size;
+    else if(!flag)
+        std::cout << str.length() - 1;
+    else
+        std::cout << -1;
+    return 0;
+}
